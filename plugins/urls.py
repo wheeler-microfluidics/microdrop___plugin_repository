f6f7619ed311a5ebe8a4274a570706fa7d51051c
Ryan Fobel <ryan@fobel.net>
2015-04-17 12:53:10 -0400

    Fix plugins latest url

--- plugins/urls.py
+++ plugins/urls.py
@@ -7,7 +7,7 @@ import app_settings
 
 urlpatterns = patterns('',
     (r'^latest/(?P<package_name>[^\/]+)(/(?P<app_major>\d+)'
-     '(\.?P<app_minor>\d+)?(.\?P<app_micro>\d+)?)?/$', views.redirect_to_latest),
+     '(\.?P<app_minor>\d+)?(\.?P<app_micro>\d+)?)?/?', views.redirect_to_latest),
     (r'^data/(?P<path>.*)$', 'django.views.static.serve',
      {'document_root': app_settings.DATA_DIR, 'show_indexes': True}),
 )
