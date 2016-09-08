fc0866646db8237480eb1dec5941e45435af506e
Christian Fobel <christian@fobel.net>
2016-09-08 10:23:52 -0400

    Auto resolve script parent directory

--- scrape_files.sh
+++ scrape_files.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
-UPDATE_SERVER_ROOT=/usr/share/plugin_repository
-#sudo chown www-data:www-data -R ${UPDATE_SERVER_ROOT}
-#sudo chmod g+rwX -R ${UPDATE_SERVER_ROOT}
+
+# Resolve parent directory of script.  See [here][1].
+#
+# [1]: http://stackoverflow.com/questions/59895/can-a-bash-script-tell-which-directory-it-is-stored-in#246128
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+PARENT_DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+UPDATE_SERVER_ROOT=${PARENT_DIR}
 
 cd ${UPDATE_SERVER_ROOT}
 DJANGO_SETTINGS_MODULE=settings python application/scripts/scrape_app_dir.py
